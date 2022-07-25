9e17ef5dd931e1478ad4c686c3ab9a2aa1a43b68
Benjamin <shallifi@hotmail.com>
2022-07-24 23:29:52 -0400

    Completed Assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        end
+    end
+
+end
